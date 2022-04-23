2fcacb083af5d7c2a64e5d4d17f143bb94360553
cuong.pham <cuong95nd@gmail.com>
2022-04-23 17:18:22 +0700

    #5 feat: add kubernetes configuration for keycloak container

--- scripts/dev/kubectl-apply.sh
+++ scripts/dev/kubectl-apply.sh
@@ -4,4 +4,7 @@ cd ../..
 kubectl apply -f deployment/kubernetes/database/k8s.yml && \
 
 kubectl apply -f deployment/kubernetes/auth/k8s.yml && \
+
+kubectl apply -f deployment/kubernetes/keycloak/k8s.yml && \
+
 kubectl apply -f deployment/kubernetes-overlays/dev/ingress/ingress.yml
